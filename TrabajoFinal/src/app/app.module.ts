24e498a696c470b6e8f684b352b9b3f604c0e7bb
Nievas2 <angelgabrielnievas@gmail.com>
2022-12-22 10:06:20 -0300

    header/banner

--- TrabajoFinal/src/app/app.module.ts
+++ TrabajoFinal/src/app/app.module.ts
@@ -3,10 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogosComponent } from './components/logos/logos.component';
+import { LogoArgProComponent } from './components/logo-arg-pro/logo-arg-pro.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogosComponent,
+    LogoArgProComponent,
+    BannerComponent,
+    SobreMiComponent
   ],
   imports: [
     BrowserModule,
